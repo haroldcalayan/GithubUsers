4e1d021885c2f52fb00934f79f1c2e87d22a8212
Harold Calayan <hcalayan@personclinic.net>
2021-01-29 21:09:35 +0800

    :sparkles: Limit network request calls to 1

--- app/src/main/java/com/haroldcalayan/githubusers/di/module/NetworkModule.kt
+++ app/src/main/java/com/haroldcalayan/githubusers/di/module/NetworkModule.kt
@@ -10,6 +10,7 @@ import com.haroldcalayan.githubusers.BuildConfig
 import com.haroldcalayan.githubusers.data.source.remote.ApiClient
 import dagger.Module
 import dagger.Provides
+import okhttp3.Dispatcher
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import javax.inject.Singleton
@@ -26,6 +27,10 @@ class NetworkModule {
     fun provideHttpClientBuilder(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient.Builder {
         var okHttpBuilder = OkHttpClient.Builder()
         okHttpBuilder.addInterceptor(httpLoggingInterceptor)
+
+        val dispatcher = Dispatcher()
+        dispatcher.maxRequests = MAXIMUM_REQUESTS_CONCURRENTLY
+
         return okHttpBuilder
     }
 
@@ -41,4 +46,8 @@ class NetworkModule {
     @Provides
     @Singleton
     fun provideApiClient(client: OkHttpClient.Builder) = ApiClient(BuildConfig.BASE_APP_URL, client)
+
+    companion object {
+        const val MAXIMUM_REQUESTS_CONCURRENTLY = 1
+    }
 }
